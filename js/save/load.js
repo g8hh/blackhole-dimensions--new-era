4cab26d62f73aa750bad0370068754ff4ece83bb
QwQe308 <78683793+QwQe308@users.noreply.github.com>
2021-11-12 09:46:23 +0800

    v0.272

--- js/save/load.js
+++ js/save/load.js
@@ -8,9 +8,19 @@ function load(save = null){
     if(localStorage.getItem(saveName)!=null) saveStr = JSON.parse(LZString.decompressFromBase64(localStorage.getItem(saveName)));
     if(save!=null) saveStr = JSON.parse(LZString.decompressFromBase64(save))
     calcPlayer();
+    //calc offline tickspeed boost
+    if(player.time && player.cheated == "no"){
+        if(Number(player.time) > new Date()){alert("检测到存档来自未来...指不准我什么时候就整了下防作弊呢( 已永久禁用离线加速.");player.cheated = "yes";player.offlineSpeedup = n(0)}
+        if(new Date() - Number(player.time) >= 10000){
+            var gain = ((Number(t.getTime()) - Number(player.time))/1000)**0.9
+            alert(`您在${formatTime((Number(t.getTime()) - Number(player.time))/1000)}的离线时间中获得了${formatTime(gain)}的离线加速!`)
+            player.offlineSpeedup = player.offlineSpeedup.add(gain)
+        }
+    }
+    player.time = Number(t.getTime())
     //window.alert("test")
     fixSave()
-    player.version = 0.27
+    player.version = 0.272
 };
 function save(){
     if(toClearSave){
--- js/support/game.js
+++ js/support/game.js
@@ -1,19 +1,29 @@
-//定义
-var t=new Date()
-var timestart=new Date()
-var diff=0
+function getTimeSpeed(force = false){
+    var tickspeed = n(1)
+    if((player.ToggleOfflineSpeedup == "on" && player.offlineSpeedup.gt(0)) || force){
+        var boost = player.offlineSpeedup.add(1).root(2).div(10).max(2)
+        if(force) return boost
+        tickspeed = tickspeed.mul(boost)
+        player.offlineSpeedup = player.offlineSpeedup.sub(boost.sub(1).mul(2).mul(diff))
+    }
+    player.offlineSpeedup = player.offlineSpeedup.max(0)
+    return tickspeed
+}
 
 function loop(){
     //deltatime-计算时间间隔
     t = new Date()
     diff=OmegaNum(Math.min((Number(t.getTime())-timestart)/1000,1/20)).mul(player.devSpeed)
+    var offlineBoost = getTimeSpeed()
+    diff=diff.mul(offlineBoost)
     timestart=t.getTime()
     //计算结束 真-输出是diff
     game(diff)
 }
 function game(diff){
     updateMain()
-    if(!player.devSpeed.eq(0)) save()
+    if(!stopSaving) if(!player.devSpeed.eq(0)) save()
+    player.time = Number(t.getTime())
 }
 
 var tickdelay = setInterval(loop,1000/30)
